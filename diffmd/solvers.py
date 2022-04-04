41f29b487410919c4219cd2b69a7b804d0efbf3d
jakublala <jakublala@gmail.com>
2022-04-04 11:21:09 +0100

    training on translational ONLY runnable

--- diffmd/solvers.py
+++ diffmd/solvers.py
@@ -35,6 +35,7 @@ class VelVerlet_NVE(FixedGridODESolver):
         NUM_VAR = 4 # vels and coords for NVE
         
         if len(state) == NUM_VAR: # integrator in the forward call 
+            print('-forward call-')
             dvdt_0, dwdt_0, dxdt_0, dqdt_0 = diffeq(state)
 
             # angular/translational velocity half-step
@@ -56,6 +57,7 @@ class VelVerlet_NVE(FixedGridODESolver):
             return tuple((v_step_full, w_step_full, x_step_full, q_step_full))
         
         elif len(state) == NUM_VAR * 2 + 1: # integrator in the backward call 
+            print('-backward call-')
             # TODO: rewrite to capture rotational kinetics
             # diffeq is the automatically generated ODE for adjoints (returns more than the original forward ODE)
             dvdt_0, dwdt_0, dxdt_0, dqdt_0, v_adj_0, w_adj_0, x_adj_0, q_adj_0, dLdpar_0  = diffeq(state) 
