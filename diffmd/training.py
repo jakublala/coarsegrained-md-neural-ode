fb3931866c3453a16b0d7355f6c26f8403842942
Jakub Lala <jakublala@gmail.com>
2022-09-08 13:21:19 +0100

    changed plot traj step

--- diffmd/training.py
+++ diffmd/training.py
@@ -244,7 +244,7 @@ class Trainer():
         else:
             batch_length = 100
             subfolder = f'results/{self.day}/{self.time}/'
-        self.training_dataset.update(batch_length, traj_step=1)
+        self.training_dataset.update(batch_length, traj_step=100)
         
         with torch.no_grad():
             # get the earliest init conditions to ensure trajectories are long enough
