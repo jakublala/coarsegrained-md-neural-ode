691a9552ecfe59462f62d2b9731ad2d238aa59cd
jakublala <jakublala@gmail.com>
2022-09-27 11:17:59 +0100

    pimped up plotting

--- analysis/energy.py
+++ analysis/energy.py
@@ -49,50 +49,6 @@ config = dict(
     )
 
 if __name__ == '__main__':
-    trainer = Trainer(config)
-
-    trajectory_index = 0
-    df = pd.read_csv(trainer.training_dataset.trajs[trajectory_index].file_path+'.csv')
-    data = trainer.training_dataset.trajs[trajectory_index].traj
-    data = [i.squeeze() for i in data]
-    v, w, x, q = data
-    potential = trainer.func.net
-
-    energies = df['potential_energy'].to_numpy()
-    
-    # batch_length = 5000
-    # trainer.training_dataset.update(batch_length, traj_step=10)
-        
-    # with torch.no_grad():
-    #     # get the earliest init conditions to ensure trajectories are long enough
-    #     batch_input, batch_y, _ = trainer.training_dataset[0]
-    #     batch_input = list(batch_input)
-    #     batch_input[0] = batch_input[0].unsqueeze(0)
-    #     batch_input = tuple(batch_input)
-
-    #     pred_y = trainer.forward_pass(batch_input, batch_length=batch_length).squeeze()
-    #     batch_y = batch_y.cpu().numpy()
-    #     batch_t = trainer.get_batch_t(batch_input[1], batch_length=batch_length).cpu().numpy())
-
-    r = x[:, 1, :] - x[:, 0, :]
-    rq = torch.cat((r, q.reshape(-1, 8)), dim=1).reshape(-1, 11).type(torch.float32)
-    
-    predicted_energies = potential(rq).detach().cpu().numpy()
-    
-    indices = np.random.randint(50, size=rq.shape[0])
-
-    import matplotlib.pyplot as plt
-    fig, ax = plt.subplots()
-    ax.plot(energies[indices], predicted_energies[indices], 'bo')
-    ax.plot([-4, 4], [-4, 4], 'k-')
-    ax.set_xlabel('Actual')
-    ax.set_ylabel('Predicted')
-    # set equal aspect ratio
-    ax.set_aspect('equal')
-    ax.set_xlim([-4, 4])
-    ax.set_ylim([-4, 4])
-    fig.savefig('energy_matrix.png')
-    plt.close(fig)
 
 
     num_steps = 500
@@ -109,22 +65,6 @@ if __name__ == '__main__':
 
 
 
-    print('before actual')
-    plt.plot(energies[:num_steps] * natoms, 'k', label='Actual')
-    # plt.plot(predicted_energies[:num_steps] - predicted_energies[0], 'r', label='Predicted')
-    plt.plot(predicted_energies[:num_steps], 'r', label='Predicted')
-
-
-    plt.ylabel('Energy')
-    plt.xlabel('Time step')
-    plt.legend()
-    plt.savefig('potential_compare.png')
-    plt.close()
-
-    k = trainer.training_dataset.trajs[trajectory_index].k
-    r0 = trainer.training_dataset.trajs[trajectory_index].r0
-    harmonic_energy = (0.5 * k * torch.square(torch.norm(r, dim=1) - r0)).detach().cpu().numpy()
-    # plt.plot(energies[:num_steps] + harmonic_energy[:num_steps], 'k--', alpha=0.5, label='Actual + Harmonic')
 
     print('before predicted')
     plt.plot(predicted_energies[:num_steps], 'r', alpha=0.5, label='Potential')
@@ -175,20 +115,3 @@ if __name__ == '__main__':
     plt.savefig('actual_energy.png')
     plt.close()
 
-
-
-
-    # ENERGIES FROM LAMMPS
-    plt.title('Logged Energies from LAMMPS')
-    plt.plot(df['total_energy'].to_numpy()[:num_steps] * natoms + harmonic_energy[:num_steps], 'k', label='Total')
-    plt.plot(df['potential_energy'].to_numpy()[:num_steps] * natoms, 'b', label='Potential')
-    plt.plot(df['kinetic_energy'].to_numpy()[:num_steps] * natoms, 'r', label='Kinetic')
-    plt.plot(harmonic_energy[:num_steps], 'g', label='Harmonic')
-    plt.legend()
-    plt.xlabel('Time step')
-    plt.ylabel('Energy')
-    plt.savefig('LAMMPS_energy.png')
-    plt.close()
-
-
-    # trainer.plot_traj()
